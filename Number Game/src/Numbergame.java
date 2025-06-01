2e9f47c0ced64ea269584a8051f0242e07f7fe24
Nikhil070103 <agrawalnikhilcoc007@gmail.com>
2025-06-01 17:56:46 +0530

    3rd task

--- Number Game/src/Numbergame.java
+++ Number Game/src/Numbergame.java
@@ -7,14 +7,14 @@ public class Numbergame {
 		
 		Scanner scanner = new Scanner(System.in);
 		Random random = new Random();
-		int randomNum = random.nextInt(100) + 1;
+		int randomNum = random.nextInt(100) ;
 		int attempts = 5;
 		int wonScore = 0;
 		
 		System.out.println("Welcome to the Number Guessing Game!");
         System.out.println("I have chosen a number between 1 to 100.");
         System.out.println("You have " + attempts + " attempts to guess it.");
-        
+    
         while(attempts > 0) {
         	
         	System.out.println("Enter Guess Number:");
@@ -26,7 +26,7 @@ public class Numbergame {
         		System.out.println("This number is correct: " + guess);
         		wonScore++;
         		
-        	}if(guess < randomNum) {
+        	}else if(guess < randomNum) {
         		
         		System.out.println("This is low!");
         	}else {
